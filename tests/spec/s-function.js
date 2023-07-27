887df2efd771b335ceeed73425256201d9ebd551
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-27 11:22:10 +0530

    Update s-function.js

--- tests/spec/s-function.js
+++ tests/spec/s-function.js
@@ -3,9 +3,9 @@ describe('Function', function () {
 
     describe('#apply()', function () {
         it('works with arraylike objects', function () {
-            var arrayLike = { length: 4, 0: 1, 2: 4, 3: true };
-            var expectedArray = [1, undefined, 4, true];
-            var actualArray = (function () {
+            let arrayLike = { length: 4, 0: 1, 2: 4, 3: true };
+            let expectedArray = [1, undefined, 4, true];
+            let actualArray = (function () {
                 return Array.prototype.slice.apply(arguments);
             }.apply(null, arrayLike));
             expect(actualArray).toEqual(expectedArray);
@@ -13,15 +13,15 @@ describe('Function', function () {
     });
 
     describe('#bind()', function () {
-        var actual;
+        let actual;
 
-        var testSubject = {
+        let testSubject = {
             push: function (o) {
                 this.a.push(o);
             }
         };
 
-        var func = function func() {
+        let func = function func() {
             Array.prototype.forEach.call(arguments, function (a) {
                 this.push(a);
             }, this);
@@ -34,7 +34,7 @@ describe('Function', function () {
         });
 
         it('binds properly without a context', function () {
-            var context;
+            let context;
             testSubject.func = function () {
                 context = this;
             }.bind();
@@ -42,7 +42,7 @@ describe('Function', function () {
             expect(context).toBe(function () { return this; }.call());
         });
         it('binds properly without a context, and still supplies bound arguments', function () {
-            var a, context;
+            let a, context;
             testSubject.func = function () {
                 a = Array.prototype.slice.call(arguments);
                 context = this;
@@ -68,11 +68,11 @@ describe('Function', function () {
             testSubject.func = function () {
                 return this;
             }.bind();
-            var context = testSubject.func();
+            let context = testSubject.func();
             expect(context).toBe(function () { return this; }.call());
         });
         it('returns properly without binding a context, and still supplies bound arguments', function () {
-            var context;
+            let context;
             testSubject.func = function () {
                 context = this;
                 return Array.prototype.slice.call(arguments);
@@ -82,92 +82,92 @@ describe('Function', function () {
             expect(actual).toEqual([1, 2, 3, 1, 2, 3]);
         });
         it('returns properly while binding a context properly', function () {
-            var ret;
+            let ret;
             testSubject.func = func.bind(actual);
             ret = testSubject.func(1, 2, 3);
             expect(ret).toBe(actual);
             expect(ret).not.toBe(testSubject);
         });
         it('returns properly while binding a context and supplies bound arguments', function () {
-            var ret;
+            let ret;
             testSubject.func = func.bind(actual, 1, 2, 3);
             ret = testSubject.func(4, 5, 6);
             expect(ret).toBe(actual);
             expect(ret).not.toBe(testSubject);
         });
         it('has the new instance\'s context as a constructor', function () {
-            var actualContext;
-            var expectedContext = { foo: 'bar' };
+            let actualContext;
+            let expectedContext = { foo: 'bar' };
             testSubject.Func = function () {
                 actualContext = this;
             }.bind(expectedContext);
-            var result = new testSubject.Func();
+            let result = new testSubject.Func();
             expect(result).toBeTruthy();
             expect(actualContext).not.toBe(expectedContext);
         });
         it('passes the correct arguments as a constructor', function () {
-            var expected = { name: 'Correct' };
+            let expected = { name: 'Correct' };
             testSubject.Func = function (arg) {
                 expect(Object.prototype.hasOwnProperty.call(this, 'name')).toBe(false);
                 return arg;
             }.bind({ name: 'Incorrect' });
-            var ret = new testSubject.Func(expected);
+            let ret = new testSubject.Func(expected);
             expect(ret).toBe(expected);
         });
         it('returns the return value of the bound function when called as a constructor', function () {
-            var oracle = [1, 2, 3];
-            var Subject = function () {
+            let oracle = [1, 2, 3];
+            let Subject = function () {
                 expect(this).not.toBe(oracle);
                 return oracle;
             }.bind(null);
-            var result = new Subject();
+            let result = new Subject();
             expect(result).toBe(oracle);
         });
 
         it('returns the correct value if constructor returns primitive', function () {
-            var Subject = function (oracle) {
+            let Subject = function (oracle) {
                 expect(this).not.toBe(oracle);
                 return oracle;
             }.bind(null);
 
-            var primitives = ['asdf', null, true, 1];
-            for (var i = 0; i < primitives.length; ++i) {
+            let primitives = ['asdf', null, true, 1];
+            for (let i = 0; i < primitives.length; ++i) {
                 expect(new Subject(primitives[i])).not.toBe(primitives[i]);
             }
 
-            var objects = [[1, 2, 3], {}, function () {}];
-            for (var j = 0; j < objects.length; ++j) {
+            let objects = [[1, 2, 3], {}, function () {}];
+            for (let j = 0; j < objects.length; ++j) {
                 expect(new Subject(objects[j])).toBe(objects[j]);
             }
         });
         it('returns the value that instance of original "class" when called as a constructor', function () {
-            var ClassA = function (x) {
+            let ClassA = function (x) {
                 this.name = x || 'A';
             };
-            var ClassB = ClassA.bind(null, 'B');
+            let ClassB = ClassA.bind(null, 'B');
 
-            var result = new ClassB();
+            let result = new ClassB();
             expect(result instanceof ClassA).toBe(true);
             expect(result instanceof ClassB).toBe(true);
         });
         it('sets a correct length without thisArg', function () {
-            var Subject = function (a, b, c) { return a + b + c; }.bind();
+            let Subject = function (a, b, c) { return a + b + c; }.bind();
             expect(Subject.length).toBe(3);
         });
         it('sets a correct length with thisArg', function () {
-            var Subject = function (a, b, c) { return a + b + c + this.d; }.bind({ d: 1 });
+            let Subject = function (a, b, c) { return a + b + c + this.d; }.bind({ d: 1 });
             expect(Subject.length).toBe(3);
         });
         it('sets a correct length with thisArg and first argument', function () {
-            var Subject = function (a, b, c) { return a + b + c + this.d; }.bind({ d: 1 }, 1);
+            let Subject = function (a, b, c) { return a + b + c + this.d; }.bind({ d: 1 }, 1);
             expect(Subject.length).toBe(2);
         });
         it('sets a correct length without thisArg and first argument', function () {
-            var Subject = function (a, b, c) { return a + b + c; }.bind(undefined, 1);
+            let Subject = function (a, b, c) { return a + b + c; }.bind(undefined, 1);
             expect(Subject.length).toBe(2);
         });
         it('sets a correct length without thisArg and too many argument', function () {
-            var Subject = function (a, b, c) { return a + b + c; }.bind(undefined, 1, 2, 3, 4);
+            let Subject = function (a, b, c) { return a + b + c; }.bind(undefined, 1, 2, 3, 4);
             expect(Subject.length).toBe(0);
         });
     });
